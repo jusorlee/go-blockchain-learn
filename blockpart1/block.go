d7e05071733601e901751a343f6293dfb79c77e2
jusorlee <jusorlee@qq.com>
2018-06-21 16:23:15 +0800

    更新代码备注

--- blockpart1/block.go
+++ blockpart1/block.go
@@ -1,51 +1,83 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"bytes"
 	"crypto/sha256"
+	"fmt"
+	"strconv"
 	"time"
 )
 
-// Block 结构体是区块信息
+//Block结构体是区块链信息
 type Block struct {
-	Timestamp     int64		//当前时间戳，也就是区块创建的时间
-	Data          []byte	//区块存储的实际有效信息，也就是交易
-	PrevBlockHash []byte	//前一个块的哈希，即父哈希
-	Hash          []byte	//当前块的哈希
+	//当前时间戳
+	Timestamp int64
+	//区块的的有消息
+	Data []byte
+	//前一个区块的HASH
+	PrevBlockHash []byte
+	//当前块的Hash
+	Hash []byte
 }
 
-// SetHash 方法计算和设置区块的hash
-func (b *Block) SetHash(){
-	//strconv.FormatInt 将一个字符串解析为整数
+//SetHash 计算和设置区块的hash
+func (b *Block) SetHash() {
+	//strconv.FormatInt  将一个字符串解析为整数
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	fmt.Println("timestamp:", timestamp)
-	
-	// 测试bytes.Join()
-	// fmt.Println(bytes.Join([][]byte{{'1'},{'3'},{'3'}},[]byte{}))
-	
-	// headers 头部信息，把前一个区块hash,当前Data,当前时间戳链接起来
-	headers := bytes.Join([][]byte{b.PrevBlockHash,b.Data,timestamp}, []byte{})
-	fmt.Println("headers:",headers)
-	
-	// 当前hash计算，把headers的组合信息，加密
-	hash := sha256.Sum256(headers)
-	fmt.Println("hash:",hash)
-	
-	//设置当前hash值
+	fmt.Println(timestamp)
+	//Println的结果：[49 53 50 55 53 55 48 57 52 49]
+
+	//测试bytes.Join()
+	//fmt.Println(bytes.Join([][]byte{{'a'}, {'3'}, {'3'}}, []byte{}))
+	//结果：[97 51 51]
+
+	//header 头部信息，把前一个区块的hash,当前Data,当前时间戳Join在一起
+	header := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	fmt.Println("header:", header)
+	//结果：header: [104 101 108 108 111 49 53 50 55 53 55 49 53 56 53]
+
+	//当前的hash计算，把header的组合信息加密
+	hash := sha256.Sum256((header))
+	fmt.Println("hash:", hash)
+	//结果：hash: [157 215 223 149 65 120 46 89 107 86 202 88 117 30 155 200 174 121 205 116 100 144 103 151 37 137 135 214 213 100 149 241]
+
+	//设置当前区块的hash值
 	b.Hash = hash[:]
-	
+
 }
 
-// NewBlock creates and returns Block
-func NewBlock(data string, PrevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}}
+//NewBlock 创建新的区块，并返回区块信息
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	fmt.Println("正在新增一个区块")
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	//设置当前区块的hash
 	block.SetHash()
+
+	//打印时间戳
+	fmt.Println("Timestamp:", block.Timestamp)
+	//打印交易信息
+	fmt.Println("Data:", block.Data)
+	//打印上一个区块的hash
+	fmt.Println("PrevBlockHash:", block.PrevBlockHash)
+	//打印当前hash
+	fmt.Println("Hash:", block.Hash)
+
+	//返回区块信息
 	return block
 }
 
-// NewGenesisBlock creates and returns genesis Block,is the first BLock
+//NewGenesisBlock创世块的生成,也就是第一个区块的生成
 func NewGenesisBlock() *Block {
-	return NewBlock("Hello,World",[]byte{})
+	fmt.Println("正在生成创世块....请耐心等待")
+	return NewBlock("这是第一个区块，也叫创世块", []byte{})
 }
+
+//这个是区块链的第一个文件代码，所以使用main函数来测试，现在测试完成，我们注释掉。
+//下面我们开始写第二个文件：把所有每一个区块连起来，变成一个列表
+/*
+func main() {
+	NewGenesisBlock()
+	NewBlock("hello", []byte{})
+
+}
+*/
--- blockpart1/blockchain.go
+++ blockpart1/blockchain.go
@@ -1,31 +1,44 @@
 package main
 
+//在这个文件里面，是把每一个区块连起来，怎么连？
+//每一个区块，都包含上一个区块的hash,就是使用这个hash将每个区块进行链接。
+
 import (
 	"fmt"
 )
 
-//Blockchain keeps a sequence of Blocks
+//Blockchain 定义一个机构体，来存储这条链
 type Blockchain struct {
+	//这里就是每个区块组成的切片（slice），这里为什么使用切片而不用数组，
+	//因为数组定义好后，是不允许再更改长度，而slice,没有这个要求
 	blocks []*Block
 }
 
-// AddBlock 在区块链中保存私有数据
-func (bc *Blockchain) AddBlock(data string) {
-	//前一个区块是当前区块的长度-1
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	fmt.Println("prevBlock:",prevBlock)
-	
-	//创建新的区块
-	newBlock := NewBlock(data,prevBlock.Hash)
-	fmt.Println("newBlock:",newBlock)
-
-	//把新建的区块添加到原区块链表的后面
-	bc.blocks = append(bc.blocks, newBlock)
-	fmt.Println(bc.blocks)
-}
-
-// NewBlockchain 创建一个新的区块链
+//NewBlockchain 创建一个新的区块链
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
 
+//AddBlock 在区块链中添加一个区块
+func (bc *Blockchain) AddBlock(data string) {
+	//前一个区块是当前区块的长度减1
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	fmt.Println("prevBlock:", prevBlock)
+
+	//当前区块的设置
+	newblock := NewBlock(data, prevBlock.Hash)
+	fmt.Println("newblock:", newblock)
+
+	//将新生成的区块，添加到列表中
+	bc.blocks = append(bc.blocks, newblock)
+	fmt.Println("bc.blocks:", bc.blocks)
+}
+
+//这个main函数也是调试用，现在我们把它抽取到一个单独的文件中
+/*
+func main() {
+	bc := NewBlockchain()
+	fmt.Println(bc)
+	bc.AddBlock("test1")
+}
+*/
